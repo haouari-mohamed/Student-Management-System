60f655435c28e99573d8498ac2d8815954600d1a
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-05-09 16:22:57 +0100

    create the student interface in DAO

---
+++ src/main/java/com/DAO/EtudiantDAO.java
@@ -0,0 +1,14 @@
+package com.DAO;
+
+import com.Beans.Etudiant;
+
+import java.util.ArrayList;
+
+public interface EtudiantDAO {
+    void Ajouter(Etudiant etudiant);
+    ArrayList<Etudiant> AfficherEtudiants();
+    void Delete(String matricule);
+    Etudiant RecupererEtudiantByMatricule(String matricule );
+    Etudiant ModifyEtudiant(String matricule , Etudiant etudiant);
+    ArrayList<Etudiant> SearchByName(String name);
+}
